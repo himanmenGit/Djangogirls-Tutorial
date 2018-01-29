56d338019171aa104067b57d934506c675c004cb
himanmen Git <study.himanmen@gmail.com>
2018-01-29 16:42:34 +0900

    blog app을 INSTALLED_APPS에 추가, Post Model의 migration생성 및 admin에 등록

--- django/blog/models.py
+++ django/blog/models.py
@@ -5,9 +5,18 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    # ForeignKey 는 다른 테이블과 연결됨 auth 테이블의 User 컬럼과 연결됨
+    author = models.ForeignKey(
+        'auth.User',
+        # django 2.0 추가 사항
+        # 유저 삭제시 연결된 테이블들의 내용을 같이 삭제
+        on_delete=models.CASCADE,
+    )
+    # CharField 는 글자수 제한 하여 저장
     title = models.CharField(max_length=200)
+    # TextField 는 글자수 제한 없이 저장
     content = models.TextField(blank=True)
+    # DateTIleField 는 날짜와 시간을 나타냄
     created_date = models.DateTimeField(
         default=timezone.now
     )
@@ -17,6 +26,7 @@ class Post(models.Model):
 
     def publish(self):
         self.published_date = timezone.now()
+        # 객체를 변환 시키고 데이터베이스에 저장해야 한다.
         self.save()
 
     def __str__(self):
