13d00be98567646a0dcf0f70bb2f7f5efd9cd752
himanmen Git <study.himanmen@gmail.com>
2018-01-30 16:51:07 +0900

    post_detail로 이동하는 링크를 url태그를 사용해서 동적으로 생성

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -11,14 +11,16 @@ from . import views
 urlpatterns = [
     # 일치하는 패턴에 대해 상응하는 모듈의
     # 함수자체를 전달
-    path('', views.post_list),
+    path('list', views.post_list, name='post-list'),
     # path('detail/', views.post_detail)
 
     # 3/
     # 53/
     # 53/asdf/ <- X
     # pk가 view.post.detail 함수에 아규먼트로 자동으로 넘어감.
-    re_path(r'(?P<pk>\d+)/$', views.post_detail)
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail)
+    path('post/<int:pk>/', views.post_detail, name='post-detail')
+
     # 숫자가 한개 이상 반복되는 경우를 정규표현식으로 구현 하되
     # 해당 반복 구간을 그룹으로 묶고, 그룹 이름을 'pk'로 지정
     # re.compile(r'(?P<pk>\d+)')
