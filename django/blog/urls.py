edc9b40b43b4d93c4d76ed3ca3ca7ec0b42aaa52
himanmen Git <study.himanmen@gmail.com>
2018-01-30 15:59:06 +0900

    post_detail에 동적으로 pk값을 URL을 통해 받을 수 있또록 처리

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+import re
+from django.urls import path, re_path
 
 # 현재 패키지에서 views.py 모듈을 불러 온다.
 # 상대 경로로 불러오는것을 권장함.
@@ -11,5 +12,14 @@ urlpatterns = [
     # 일치하는 패턴에 대해 상응하는 모듈의
     # 함수자체를 전달
     path('', views.post_list),
-    path('detail/', views.post_detail)
+    # path('detail/', views.post_detail)
+
+    # 3/
+    # 53/
+    # 53/asdf/ <- X
+    # pk가 view.post.detail 함수에 아규먼트로 자동으로 넘어감.
+    re_path(r'(?P<pk>\d+)/$', views.post_detail)
+    # 숫자가 한개 이상 반복되는 경우를 정규표현식으로 구현 하되
+    # 해당 반복 구간을 그룹으로 묶고, 그룹 이름을 'pk'로 지정
+    # re.compile(r'(?P<pk>\d+)')
 ]
--- django/blog/views.py
+++ django/blog/views.py
@@ -37,7 +37,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', context)
 
 
-def post_detail(request):
+def post_detail(request, pk):
     """
     localhost:8000/detail/로 온 요청을
     'blog/post_detail.html'을 render한 결과를 리턴
@@ -45,7 +45,12 @@ def post_detail(request):
     ursl, views, template을 모두 작성 해야 함
 
     :param request: request요청
+    :param pk: primary key
     :return: blog/post_detail.html
     """
 
-    return render(request, 'blog/post_detail.html')
+    context = {
+        'post': Post.objects.get(pk=pk),
+    }
+
+    return render(request, 'blog/post_detail.html', context)
